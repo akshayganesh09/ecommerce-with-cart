dd1aae120eb5693eaa44e16ea4d93b44e4eea690
Akshay Ganesh <iakshayganesh98@gmail.com>
2026-03-02 01:02:21 +0530

    cart calculation

---
+++ src/controller/cart.controller.js
@@ -0,0 +1,27 @@
+const cartService = require("../services/cart.services");
+const { sendSuccess, sendError } = require("../utils/response.utils");
+
+const addToCart = (req, res, next) => {
+    try {
+        const { productId, quantity } = req.body;
+
+        const cart = cartService?.addToCart({ productId, quantity});
+        return sendSuccess(res, cart, 200, "Item added to cart.");
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getCart = (req, res, next) => {
+    try {
+        const cart = cartService?.getCart();
+        const total = cartService?.calculateTotal();
+        console.log("total:", total);
+
+        return sendSuccess(res, { items: cart, total: total });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { addToCart, getCart };
---
+++ src/services/cart.services.js
@@ -0,0 +1,40 @@
+const productService = require("../services/product.services");
+
+let cart = [];
+
+const addToCart = (payload) => {
+    const product = productService?.getProductById(payload?.productId);
+
+    if(!product) {
+        throw new Error("Product does not exist");
+    }
+
+    const existingItem = cart?.find(item => item.id === payload?.productId);
+
+    if (existingItem) {
+        existingItem.quantity += payload?.quantity;
+    } else {
+        cart?.push({
+            id: product?.id,
+            name: product?.name,
+            price: product?.price,
+            quantity: payload?.quantity
+        });
+    }
+
+    return cart;
+};
+
+const getCart = () => {
+    return cart;
+}
+
+const calculateTotal = () => {
+    const total = cart.reduce((total, item) => {
+        return total + item?.quantity * item?.price
+    }, 0);
+
+    return total;
+};
+
+module.exports = { addToCart, getCart, calculateTotal };
