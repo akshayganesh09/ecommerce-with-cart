1f45ad72d3de5590a24a559a006e6318b3709703
Akshay Ganesh <iakshayganesh98@gmail.com>
2026-03-06 16:26:42 +0530

    enable sorting

--- src/services/product.services.js
+++ src/services/product.services.js
@@ -1,8 +1,51 @@
 const pool = require("../config/db");
 
-const getAllProducts = async (page = 1, limit = 10) => {
+const getAllProducts = async (page = 1, limit = 10, filter = {}) => {
+    console.log("filter: ", filter);
     const offSet = (page - 1) * limit;
-    const result = await pool.query("SELECT * FROM products ORDER BY id LIMIT $1 OFFSET $2", [limit, offSet]);
+
+    let query = `SELECT * FROM products`;
+    const condition = [];
+    const values = [];
+
+    let index = 1;
+
+    // Filter with minPrice.
+    if (filter.minPrice) {
+        condition.push(`price >= $${index}`);
+        values.push(filter.minPrice);
+        index++;
+    };
+
+    // Filter with maxPrice.
+    if (filter.maxPrice) {
+        condition.push(`price <= $${index}`);
+        values.push(filter.maxPrice);
+        index++;
+    }
+
+    //Check if any condition exist.
+    if (condition?.length > 0) {
+        query = query + ` WHERE ` + condition.join(" AND ");
+    }
+
+    // Sorting.
+    if (filter.sort) {
+        console.log(filter.sort)
+        const order = filter.sort.startsWith("-") ? "DESC" :"ASC";
+        const field = filter.sort.replace("-", "");
+
+        query = query + ` ORDER BY ${field} ${order}`;
+    } else {
+        query = query + ` ORDER BY id`;
+    }
+
+    // Pagenation.
+    query = query + ` LIMIT $${index} OFFSET $${index + 1}`;
+    values.push(limit, offSet);
+
+    // Apply the query and values to fetch the data from the Table.
+    const result = await pool.query(query, values);
 
     const countResult = await pool.query("SELECT COUNT(*) FROM products");
 
